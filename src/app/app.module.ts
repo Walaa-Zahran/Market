db30e0f91e6fff052d4efa28b66f7fa20b846101
walaazahran <walaazahran40@gmail.com>
2023-06-09 00:21:34 +0300

    angular-structure , update project design ,api,env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartsComponent } from './carts/components/carts/carts.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CartsComponent } from './carts/components/carts/carts.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
